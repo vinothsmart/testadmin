8a494261b13a7af9a3c1aa1ad2d80126d4ed5cd2
vinothkanna <s.vinothkanna90@gmail.com>
2020-03-03 16:32:34 +0530

    Loading posts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Admin, Resource } from 'react-admin';
+import { Admin, Resource, ListGuesser } from 'react-admin';
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './users';
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={ListGuesser} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
