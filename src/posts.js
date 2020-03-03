84272a2057d9b2a37207731663573d694686cdd6
vinothkanna <s.vinothkanna90@gmail.com>
2020-03-03 17:38:36 +0530

    added title in react

--- src/posts.js
+++ src/posts.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { List, Datagrid, TextField, ReferenceField, EditButton, Edit, SimpleForm, TextInput, ReferenceInput, SelectInput, Create } from 'react-admin';
 
+const PostTitle = ({ record }) => {
+    return <span>Post {record ? `"${record.title}"` : ''}</span>;
+};
+
 export const PostList = props => (
     <List {...props}>
         <Datagrid>
@@ -15,7 +19,7 @@ export const PostList = props => (
 );
 
 export const PostEdit = props => (
-    <Edit {...props}>
+    <Edit title={<PostTitle />} {...props}>
         <SimpleForm>
            <TextInput disabled source="id" />
             <ReferenceInput source="userId" reference="users">
