bca94a724d4f96b7de88ea9fbd97219bf4da192c
vinothkanna <s.vinothkanna90@gmail.com>
2020-03-10 14:59:56 +0530

    custom api pending

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Admin, Resource } from 'react-admin';
-// import jsonServerProvider from 'ra-data-json-server';
+import jsonServerProvider from 'ra-data-json-server';
 import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import Dashboard from './Dashboard';
 import authProvider from './authProvider';
-import dataProvider from './dataProvider';
+// import dataProvider from './dataProvider';
 
-// const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
 const App = () => (
   <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
